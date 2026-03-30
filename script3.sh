15382976ab72bd072765dfbe1158fe1c3ffadbd3
Adarsh patel <billgate00b@gmail.com>
2026-03-31 03:05:58 +0530

    added comment

--- script3.sh
+++ script3.sh
@@ -6,10 +6,11 @@ DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 echo "Directory Audit Report"
 echo "--------------------------------"
 
-for DIR in "${DIRS[@]}"; do
-    if [ -d "$DIR" ]; then
-        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
-        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+for DIR in "${DIRS[@]}"; do # DIRS[@] expands all array elements
+    if [ -d "$DIR" ]; then  #-d checks if path is a directory
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}') # awk extracts permission
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1) #du -sh gets humnan-readable size
+        #cut -f1 removes path
         echo "$DIR => Permissions: $PERMS | Size: $SIZE"
     else
         echo "$DIR does not exist"
